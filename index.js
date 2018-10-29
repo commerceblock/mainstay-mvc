4f40f02d5eb2818adb4ed23c85027295e2808f77
lodenos <nikolaiilodenos@gmail.com>
2018-10-29 23:15:41 +0000

    add api routes

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+var api = require('./lib/apiroutes')
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('{"response":"Mainstay-API"}');
-});
-
+const API_INDEX = '/'
+const API_LATEST_ATTESTATION = '/api/latestattestation'
+const API_LATEST_COMMITMENT = '/api/latestcommitment'
+const API_COMMITMENT_LATEST_PROOF = '/api/commitment/latestproof'
+const API_COMMITMENT_PROOF = '/api/commitment/proof'
+const API_COMMITMENT_SEND = '/api/commitment/send'
+const API_COMMITMENT_VERIFY = '/api/commitment/verify'
+// Get Routes for METHOD GET
+app.get(API_INDEX, api.index);
+app.get(API_LATEST_ATTESTATION, api.latest_attestation);
+app.get(API_LATEST_COMMITMENT, api.latest_commitment);
+app.get(API_COMMITMENT_LATEST_PROOF, api.commitment_latest_proof);
+app.get(API_COMMITMENT_VERIFY, api.commitment_verify);
+app.get(API_COMMITMENT_PROOF, api.commitment_proof);
+// Get Routes for METHOD POST
+app.post(API_COMMITMENT_SEND, api.commitment_send);
+//
 app.listen(9000);
