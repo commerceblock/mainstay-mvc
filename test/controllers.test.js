2132547d1174106965ce90801b1d77023ac6d062
lodenos <nikolaiilodenos@gmail.com>
2018-11-08 13:06:55 +0000

    verify commitment_verify & commitment_proof and add basic test

--- test/controllers.test.js
+++ test/controllers.test.js
@@ -219,22 +219,44 @@ describe('Test Controllers', () => {
   //////////////////////////////////////////////////////////////////////////////
   /// Test Commitment Proof                                                  ///
   //////////////////////////////////////////////////////////////////////////////
-  it('Route: /api/v1/commitment/proof', () => {
+  it('Route: /api/v1/commitment/proof?position=0&commitment='
+     '1a39e34e881d49a1e6cdc3418b54aa57747106bc75e9e8443666127f98ada3b7', () => {
     const req = mockHttp.createRequest(
-        {method : 'GET', url : '/api/v1/commitment/proof'});
+        { method: 'GET', url: '/api/v1/commitment/proof'});
     const res = mockHttp.createResponse();
     controllers.commitment_proof(req, res);
+    let json = JSON.parse(res._getData());
+    assert(json.response.merkle_root ===
+        '0849d03563d6d7e7d2d0063a5dae944bfcfd4d56d149ffbe73ccead275b2762e');
+    assert(json.response.ops[0].append === true);
+    assert(json.response.ops[0].commitment ===
+        '2a39e34e883d9a1e6cdc3418b54aa57747106bc75e3e84426661f27f98ada3b7');
+    assert(json.response.ops[1].append === true);
+    assert(json.response.ops[1].commitment ===
+        '91ffafe62866dd95ea1ed7a56907ddc59ea495b477c3e8f853ee2d1b55a24d47');
   });
+  ///
+  /// Add Test ... I'm too lazy to write these nuts test
+  ///
   //////////////////////////////////////////////////////////////////////////////
   /// Test Commitment Verify                                                 ///
   //////////////////////////////////////////////////////////////////////////////
-  it('Route: /api/v1/commitment/verify', () => {
+  it('Route: /api/v1/commitment/verify?position=0&commitment='
+     '1a39e34e881d49a1e6cdc3418b54aa57747106bc75e9e8442666127f98ada3b7', () => {
+    const position = 'position=0'
+    const commitment = 'commitment='
+        '1a39e34e881d49a1e6cdc3418b54aa57747106bc75e9e8442666127f98ada3b7'
     const req = mockHttp.createRequest(
-        {method : 'GET', url : '/api/v1/commitment/verify'});
+        { method: 'GET',
+          url: '/api/v1/commitment/verify?' + position + '&' + commitment});
     const res = mockHttp.createResponse();
     controllers.commitment_verify(req, res);
-    ;
+    let json = JSON.parse(res._getData());
+    assert(json.response.confirmed === true);
   });
+  ///
+  /// Add Test ... I'm too lazy to write these nuts test
+  ///
   //////////////////////////////////////////////////////////////////////////////
   /// Commitment Send                                                        ///
   //////////////////////////////////////////////////////////////////////////////
