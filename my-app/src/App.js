66a9740a1c42f8ffb7e019d46711759cf1db9105
lodenos <nikolaiilodenos@gmail.com>
2018-11-19 14:05:36 +0000

    add websocket for priceCBT

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Sockette from 'sockette'
 
 const FaviconAddrBar = () => {
   return (
@@ -81,15 +82,51 @@ const MarketcapCBT = () => {
   );
 }
 
-const PriceCBT = () => {
-  return (
-    <div class="mb-3 flex-table">
-      <div class="d-flex justify-content-end header">
-        <span class="lh1rem mr-auto">Price</span>
-        <span class="lh1rem text-right ml-1">0.012686 $</span>
+class PriceCBT extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      channel: 0,
+      priceCBT: 0,
+      ws: new Sockette('wss://api.ethfinex.com/ws/2', {
+        timeout: 5e3,
+        maxAttempts: 10,
+        onopen: e => console.log('Connected!', e),
+        onmessage: e => this.message(e.data),
+        onreconnect: e => console.log('Reconnecting...', e),
+        onmaximum: e => console.log('Stop Attempting!', e),
+        onclose: e => console.log('Closed!', e),
+        onerror: e => console.log('Error:', e)
+      })
+    }
+  }
+
+  message(data) {
+    data = JSON.parse(data);
+    if (data.event === "info")
+      this.state.ws.send('{"event":"subscribe","channel":"trades","symbol":"tCBTUSD"}');
+    if (data.event === "subscribed" && data.channel === "trades")
+      this.setState({channel: data.chanId});
+    if (Array.isArray(data)) {
+      if (Array.isArray(data[1])) {
+        if (data[0] === this.state.channel)
+          this.setState({priceCBT: data[1][0][3]});
+      } else if (data[1] === 'te' || data[1] === 'tu')
+        if (data[0] === this.state.channel)
+          this.setState({priceCBT: data[2][3]});
+    }
+  }
+
+  render() {
+    return (
+      <div class="mb-3 flex-table">
+        <div class="d-flex justify-content-end header">
+          <span class="lh1rem mr-auto">Price</span>
+          <span class="lh1rem text-right ml-1">{this.state.priceCBT} $</span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 const InfoLatestTxid = () => {
@@ -180,7 +217,7 @@ const listAttestation = {
   ]
 }
 
-class LatestAttestation extends Component {
+class LatestAttestation extends React.Component {
   render() {
   return (
     <div>
@@ -224,7 +261,7 @@ const listCommitment = {
   ]
 }
 
-class LatestCommitment extends Component {
+class LatestCommitment extends React.Component {
   render() {
   return (
     <div>
@@ -270,7 +307,7 @@ const FooterPage = () => {
   );
 }
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="top-nav">
