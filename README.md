0ded0f9e4582cd5a5ba48c9e5a986de8af0f94a2
lodenos <nikolaiilodenos@gmail.com>
2018-12-07 12:57:53 +0000

    sesolve greenkeeper #35

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ mongorestore -d mainstayX dbTest/
 
 run example
 ```perl
-$ DB_NAME="mainstayX" DB_PORT="27017" DB_HOST="127.0.0.1" LISTEN_API="4000" node ./src/api.js
+$ DB_NAME="mainstayX" DB_PORT="27017" DB_HOST="127.0.0.1" LISTEN_API="4000" node ./src/app.js
 $ HOST_API="localhost" PORT_API="4000" PORT="80" webpack-dev-server
 ```
---
+++ src/routes/api.js
@@ -0,0 +1,33 @@
+const controllers = require('../controllers/controllers');
+
+const API_INDEX = '/api/v1'
+const API_LATEST_ATTESTATION = '/api/v1/latestattestation'
+const API_ATTESTATION = '/api/v1/attestation'
+const API_LATEST_COMMITMENT = '/api/v1/latestcommitment'
+const API_COMMITMENT = '/api/v1/commitment'
+const API_COMMITMENT_LATEST_PROOF = '/api/v1/commitment/latestproof'
+const API_COMMITMENT_PROOF = '/api/v1/commitment/proof'
+const API_COMMITMENT_SEND = '/api/v1/commitment/send'
+const API_COMMITMENT_VERIFY = '/api/v1/commitment/verify'
+
+function api_get(app) {
+  app.get(API_INDEX, controllers.index);
+  app.get(API_LATEST_ATTESTATION, controllers.latest_attestation);
+  app.get(API_ATTESTATION, controllers.attestation);
+  app.get(API_LATEST_COMMITMENT, controllers.latest_commitment);
+  app.get(API_COMMITMENT, controllers.commitment);
+  app.get(API_COMMITMENT_LATEST_PROOF, controllers.commitment_latest_proof);
+  app.get(API_COMMITMENT_VERIFY, controllers.commitment_verify);
+  app.get(API_COMMITMENT_PROOF, controllers.commitment_proof);
+}
+
+function api_post(app) {
+  app.post(API_COMMITMENT_SEND, controllers.commitment_send);
+}
+
+function api(app) {
+  api_get(app);
+  api_post(app);
+}
+
+module.exports.api = api;
