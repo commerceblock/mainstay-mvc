e273fc18ca31eaee025bd06cdff5e339d6e63301
Vardan Alaverdyan <vard.alaverdyan@gmail.com>
2019-01-07 00:13:53 +0400

    Menu bar + attestations page

--- src/view.jsx
+++ src/view.jsx
@@ -1,8 +1,10 @@
 import Home from './view/home/home';
 import Search from './view/search/search'
+import Attestation from './view/attestation';
+
 import {
-  BrowserRouter,
-  Route
+    BrowserRouter,
+    Route
 } from 'react-router-dom'
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -11,12 +13,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/stylesheets/styles.scss'
 
 const Main = () => (
-  <BrowserRouter>
-    <div>
-      <Route path="/" exact component={Home}/>
-      <Route path="/search" exact component={Search}/>
-    </div>
-  </BrowserRouter>
+    <BrowserRouter>
+        <div>
+            <Route path="/" exact component={Home}/>
+            <Route path="/search" exact component={Search}/>
+            <Route path="/attestation" exact component={Attestation}/>
+        </div>
+    </BrowserRouter>
 );
 
 ReactDOM.render(<Main/>, document.getElementById('root'));
+
+module.hot.accept();
