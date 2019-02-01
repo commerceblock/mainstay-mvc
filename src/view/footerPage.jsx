3614b3982155a39277ebfad9471c4589c8f2d871
Vardan Alaverdyan <vard.alaverdyan@gmail.com>
2019-02-01 10:27:19 +0400

    Front: policy and T&C #93

--- src/view/footerPage.jsx
+++ src/view/footerPage.jsx
@@ -8,6 +8,8 @@ class FooterPage extends Component {
           <div>
             <a class="nav-item" href="https://github.com/commerceblock/mainstay-mvc">Mainstay-mvc v0.3.0-release</a>
             <a class="nav-item" href="https://github.com/commerceblock/mainstay-mvc/blob/develop/LICENSE">© 2019 CommerceBlock-Limited</a>
+            <a class="nav-item" href="https://github.com/commerceblock/mainstay-mvc/blob/develop/doc/mainstay-privacy-policy.docx">Privacy Policy</a>
+            <a class="nav-item" href="https://github.com/commerceblock/mainstay-mvc/blob/develop/doc/mainstay-t%26c-DRAFT.docx">Terms and Conditions</a>
           </div>
         </div>
       </footer>
