f680eabdf844b8a292da850a0f020d16140796bc
Vardan Alaverdyan <vard.alaverdyan@gmail.com>
2019-02-05 12:27:33 +0400

    Terms and conditions (new pages)

--- src/view/footerPage.jsx
+++ src/view/footerPage.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 class FooterPage extends Component {
     render() {
         return (
@@ -8,8 +9,8 @@ class FooterPage extends Component {
           <div>
             <a class="nav-item" href="https://github.com/commerceblock/mainstay-mvc">Mainstay-mvc v0.3.0-release</a>
             <a class="nav-item" href="https://github.com/commerceblock/mainstay-mvc/blob/develop/LICENSE">© 2019 CommerceBlock-Limited</a>
-            <a class="nav-item" href="https://github.com/commerceblock/mainstay-mvc/blob/develop/doc/mainstay-privacy-policy.docx">Privacy Policy</a>
-            <a class="nav-item" href="https://github.com/commerceblock/mainstay-mvc/blob/develop/doc/mainstay-t%26c-DRAFT.docx">Terms and Conditions</a>
+            <a class="nav-item" href="/privacy" target="_blank">Privacy Policy</a>
+            <a class="nav-item" href="/terms" target="_blank">Terms and Conditions</a>
           </div>
         </div>
       </footer>
