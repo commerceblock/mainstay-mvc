690fe604059cbefddc8f3b55a48bf7529294d9c7
nkostoulas <nkostoulas@gmail.com>
2019-02-08 13:53:37 +0000

    update release

--- src/view/footerPage.jsx
+++ src/view/footerPage.jsx
@@ -7,7 +7,7 @@ class FooterPage extends Component {
             <footer class="navbar-fixed-bottom">
         <div class="text-center">
           <div>
-            <a class="nav-item" href="https://github.com/commerceblock/mainstay-mvc">Mainstay-mvc v0.3.0-release</a>
+            <a class="nav-item" href="https://github.com/commerceblock/mainstay-mvc">Mainstay-mvc v0.4.0-release</a>
             <a class="nav-item" href="https://github.com/commerceblock/mainstay-mvc/blob/develop/LICENSE">© 2019 CommerceBlock-Limited</a>
             <a class="nav-item" href="/privacy" target="_blank">Privacy Policy</a>
             <a class="nav-item" href="/terms" target="_blank">Terms and Conditions</a>
