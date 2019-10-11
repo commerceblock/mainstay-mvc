a28e559cae85af65d2dd83c011e45fe6876ada6e
Vardan Alaverdyan <vard.alaverdyan@gmail.com>
2019-10-11 23:54:38 +0400

    179: create component and add menu item

---
+++ src/view/admin/pages/ClientSignUpList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+class ClientSignUpList extends React.Component {
+
+    render() {
+        return <div>ewfsdax</div>;
+    }
+}
+
+export default connect()(ClientSignUpList);
