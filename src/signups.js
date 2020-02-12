e398481ac34264950a9a1ed0a4c76031238279b3
nkostoulas <nkostoulas@gmail.com>
2020-02-12 16:28:22 +0000

    New signups service

---
+++ src/signups.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const mongoose = require('mongoose');
+const env = require('../src/env');
+
+function connect_mongo() {
+    let url = 'mongodb://';
+    if (env.db.user && env.db.password) {
+        url += env.db.user + ':' + env.db.password;
+    }
+    url = url + '@' + env.db.address + ':' + env.db.port + '/' + env.db.database;
+    mongoose.connect(url, {useNewUrlParser: true});
+    return mongoose.connection;
+}
+
+function do_work() {
+    const db = connect_mongo();
+    mongoose.set('debug', true);
+    db.on('disconnected', function (ref) {
+        console.log('Connection with database lost');
+        process.exit(1);
+    });
+    db.on('error', console.error.bind(console, 'Connection Error:'));
+    db.once('open', () => {
+        console.log("Signups service running...")
+    });
+}
+
+do_work();
