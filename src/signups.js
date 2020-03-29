99b52c5d8ca0660b853004b47864ee9dcd833d69
Vardan Alaverdyan <vard.alaverdyan@gmail.com>
2020-03-29 13:30:57 +0400

    171: create a separate file for sending emails

--- src/signups.js
+++ src/signups.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const path = require('path');
-const nodemailer = require('nodemailer');
 const axios = require('axios');
 const mongoose = require('mongoose');
 const env = require('../src/env');
@@ -110,110 +107,13 @@ async function check_kyc_status(signup) {
     return await signup.save();
 }
 
-/**
- * create email transport
- * @returns {*}
- */
-function get_mail_transport() {
-    return nodemailer.createTransport(env.mail_server.smtp);
-}
-
-/**
- * send email
- * @param signup
- * @returns {Promise<unknown>}
- */
-function send_signup_email(signup) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path.resolve(__dirname, './view/emails/signup/client.html'), 'utf8', (error, html) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(html);
-            }
-        });
-    }).then((html) => {
-
-        const cbLogo = {
-            filename: 'logo-commerce-block',
-            path: path.resolve(__dirname, '../public/logo-commerce-block.png'),
-            cid: 'logo-commerce-block.png',
-            contentType: 'image/png'
-        };
-        const msLogo = {
-            filename: 'logo-main-stay',
-            path: path.resolve(__dirname, '../public/logo-main-stay.png'),
-            cid: 'logo-main-stay.png',
-            contentType: 'image/png'
-        };
-
-        html = html.replace('$$NAME$$', signup.first_name + ' ' + signup.last_name);
-        html = html.replace('$$COMMERCE-BLOCK-LOGO-URL$$', `cid:${cbLogo.cid}`);
-        html = html.replace('$$MAIN-STAY-LOGO-URL$$', `cid:${msLogo.cid}`);
-
-        return new Promise((resolve, reject) => {
-            get_mail_transport().sendMail({
-                from: {
-                    name: 'Mainstay Support',
-                    address: 'support@mainstay.xyz'
-                },
-                to: signup.email,
-                subject: 'MainStay - Thank you for signing up! Here are the next steps',
-                html: html,
-                attachments: [cbLogo, msLogo],
-            }, (error, info) => {
-                if (error) {
-                    return reject(error);
-                }
-                console.log('email sent to ' + signup.email);
-                resolve(info);
-            });
-        });
-    }).catch(error => {
-        console.log('error while sending an email to ' + signup.email);
-        console.error(error);
-    });
-}
-
-/**
- * send email
- * @param user
- * @returns {Promise<unknown>}
- */
-function send_new_signup_mail(user) {
-    const html = `
-        <b>First Name</b>: ${user.first_name}<br>
-        <b>Last Name</b>: ${user.last_name}<br>
-        <b>Email</b>: ${user.email}<br>
-        ${user.company ? `<b>Company</b>: ${user.company}<br>` : ''}
-        ${user.pubkey ? `<b>Public Key</b>: ${user.pubkey}<br>` : ''}
-    `;
-
-    return new Promise((resolve, reject) => {
-        get_mail_transport().sendMail({
-            from: {
-                name: env.mail_server.smtp.from_name,
-                address: env.mail_server.smtp.from_address
-            },
-            to: env.sign_up.admin_email,
-            subject: 'New SignUp',
-            html: html,
-        }, (error, info) => {
-            if (error) {
-                return reject(error);
-            }
-            resolve(info);
-        });
-    });
-}
-
 async function start_kyc(signup) {
     console.log('New signup found: ' + signup.first_name + ' ' + signup.last_name);
 
     // send email to admin
-    await send_new_signup_mail(signup);
+    await EmailHelper.sendNewSignupMail(signup);
     // send email
-    await send_signup_email(signup);
+    await EmailHelper.sendSignupEmail(signup);
 
     const applicant = await send_kyc(signup);
     const kycId = applicant.id;
